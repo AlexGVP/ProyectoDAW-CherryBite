d4a9418afdf3714987e436462438bb515716e317
AlexGVP <i202122156@cibertec.edu.pe>
2024-06-28 05:33:57 -0500

    desesperación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
+import { HomeComponent } from './dashboard/home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: LoginComponent},
+    {path: "dashboard", component: DashboardComponent,
+        canActivate: [authGuard],
+        children: [
+            {path: "home", component: HomeComponent},
+        ]
+    },
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path: "**", component: PageNotFoundComponent}
+];
