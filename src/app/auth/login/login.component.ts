a5da63140be78661a17ff18e6a48ab218c8c3481
AlexGVP <i202122156@cibertec.edu.pe>
2024-07-06 16:56:39 -0500

    a

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -19,24 +19,20 @@ export class LoginComponent {
   ) { }
   
   navigateToHome(event: Event): void {
-    event.preventDefault(); // Previene la recarga de la página
-    this.router.navigate(['/']); // Navega a la ruta principal
+    event.preventDefault();
+    this.router.navigate(['/']);
   }
   
   submit(usuario: HTMLInputElement, password: HTMLInputElement): void {
     this.authService.login(usuario.value, password.value).subscribe({
       next: (response) => {
-        // Guarda el token y otros datos necesarios en sessionStorage o localStorage
         sessionStorage.setItem('token', response.token);
         sessionStorage.setItem('isLogged', 'true');
-        // Redirige al usuario a la página principal o dashboard
         this.router.navigateByUrl('/dashboard');
       },
       error: (err) => {
-        // Maneja el error de autenticación aquí
         alert(err.error.message);
       }
     });
   }
-
 }
